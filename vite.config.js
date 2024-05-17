545497df39eb63d4c0b4c642ba3a5d435fc02f27
shun916 <118881278+code-polaris044@users.noreply.github.com>
2024-05-17 18:22:29 +0900

    [fix:]

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: './src', //開発ディレクトリ設定
+    root: './src',
     build: {
-        outDir: '../dist', //出力場所の指定
+        outDir: '../dist',
         rollupOptions: {
-            //ファイル出力設定
-            output: {
-                assetFileNames: (assetInfo) => {
-                    let extType = assetInfo.name.split('.')[1];
-                    //Webフォントファイルの振り分け
-                    if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
-                        extType = 'fonts';
-                    }
-                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-                        extType = 'images';
-                    }
-                    //ビルド時のCSS名を明記してコントロールする
-                    if (extType === 'css') {
-                        return `assets/css/style.css`;
-                    }
-                    return `assets/${extType}/[name][extname]`;
-                },
-                chunkFileNames: 'assets/js/[name].js',
-                entryFileNames: 'assets/ts/[name].ts',
+            input: {
+                main: './src/index.html', // メインのHTMLエントリーポイント
+                style: './src/sass/style.scss', // SCSSエントリーポイント
             },
-        },
-        css: {
-            minify: false, // CSSの最小化を無効にする
         },
     },
 });
