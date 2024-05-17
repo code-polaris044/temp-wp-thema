5dd484238c2d09944917ac7ed8afa587f5628309
shun916 <118881278+code-polaris044@users.noreply.github.com>
2024-05-17 17:29:45 +0900

    [add:]vite&ts

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,16 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import js from '@eslint/js';
 import typescriptEslint from 'typescript-eslint';
 
+// jsを使う場合、コメントアウトを解除
+
 export default [
     eslintConfigPrettier,
-    {
-        files: ['src/js/*.js'],
-        rules: {
-            ...js.configs.recommended.rules,
-        },
-    },
+    // {
+    //     files: ['src/js/*.js'],
+    //     rules: {
+    //         ...js.configs.recommended.rules,
+    //     },
+    // },
     {
         files: ['src/ts/*.ts'],
         rules: {
@@ -17,20 +19,20 @@ export default [
             ...typescriptEslint.configs.recommended.rules,
         },
     },
-    {
-        files: ['src/js/*.js'],
-        rules: {
-            ...js.configs.recommended.rules,
-            'no-unused-vars': 'warn',
-        },
-    },
-    {
-        files: ['src/js/*.js'],
-        rules: {
-            ...js.configs.all.rules,
-            'no-unused-vars': 'warn',
-        },
-    },
+    // {
+    //     files: ['src/js/*.js'],
+    //     rules: {
+    //         ...js.configs.recommended.rules,
+    //         'no-unused-vars': 'warn',
+    //     },
+    // },
+    // {
+    //     files: ['src/js/*.js'],
+    //     rules: {
+    //         ...js.configs.all.rules,
+    //         'no-unused-vars': 'warn',
+    //     },
+    // },
     {
         files: ['src/ts/*.ts'],
         rules: {
